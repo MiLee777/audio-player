8c4b8aece3d2a7ead189ed535a03b4a8354a0982
MiLee777 <midyashka1506@gmail.com>
2023-09-24 02:46:04 +0300

    feat: add style and functional

---
+++ audio-player/playList.js
@@ -0,0 +1,30 @@
+const playList = [
+    {
+        name: 'Merry go round of life',
+        artist: 'Grissini Project',
+        img: '../assets/images/howl-s-moving-castle.jpg',
+        src: '../assets/music/Howls Moving Castle.mp3',
+        duration: '06:07'
+    },
+    {
+        name: 'One summer`s day',
+        artist: 'Joe Hisaishi',
+        img: '../assets/images/spirited-away.jpg',
+        src: '../assets/music/Spirited Away.mp3',
+        duration: '03:38'
+    },
+    {
+        name: 'Path of the wind',
+        artist: 'Joe Hisaishi',
+        img: '../assets/images/totoro.jpg',
+        src: '../assets/music/Totoro.mp3',
+        duration: '03:40'
+    },
+    {
+        name: 'A town with an ocean view',
+        artist: 'Joe Hisaishi',
+        img: '../assets/images/kiki-s-delivery-service.jpg',
+        src: '../assets/music/Kikis Delivery Service.mp3',
+        duration: '04:01'
+    }
+]
